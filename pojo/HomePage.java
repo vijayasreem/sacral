39e98c87e4fa0e25eb63733b0366412cfd8f355c
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-05-12 11:49:29 +0530

    initial commit

---
+++ pojo/HomePage.java
@@ -0,0 +1,12 @@
+public class HomePage {
+    private String username;
+    private String password;
+    
+    public void login() {
+        // code to log in to sacral.ai website
+    }
+    
+    public void accessHomePage() {
+        // code to access the Home page and various products
+    }
+}
