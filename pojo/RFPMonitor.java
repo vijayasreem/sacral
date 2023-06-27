9fe3b25536cd708c79defb8723773c3a345ba85c
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-06-27 11:29:23 +0530

    initial commit

---
+++ pojo/RFPMonitor.java
@@ -0,0 +1,38 @@
+public class RFPMonitor {
+
+    private String document;
+    private List<String> changesHistory;
+
+    public RFPMonitor(String document) {
+        this.document = document;
+        this.changesHistory = new ArrayList<>();
+    }
+
+    public void monitorChanges() {
+        // Implementation to monitor and detect changes to the RFP document
+    }
+
+    public List<String> getChangesHistory() {
+        return changesHistory;
+    }
+
+    public void viewChangesInRealTime() {
+        // Implementation to view changes to the RFP document in real-time
+    }
+
+    public void viewChangesInSecureManner() {
+        // Implementation to view changes to the RFP document in a secure manner
+    }
+
+    public void viewChangesFromAnyDevice() {
+        // Implementation to view changes to the RFP document from any device
+    }
+
+    public void viewChangesWithoutAnyLagOrDelay() {
+        // Implementation to view changes to the RFP document without any lag or delay
+    }
+
+    public void viewChangesInEasyToUnderstandFormat() {
+        // Implementation to view changes to the RFP document in an easy to understand format
+    }
+}
